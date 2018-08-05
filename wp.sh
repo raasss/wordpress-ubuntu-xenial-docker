9f301ee0758a63ea5f19da99df5d9ae947f67ad8
Radisa Jovicevic <radisa.jovicevic@tamedia.ch>
2018-08-05 23:47:41 +0200

    run wp cli in php service as runasuser

--- wp.sh
+++ wp.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-source include/create-env-file.sh
-
-docker-compose exec php wp --path=/var/www/html --allow-root "$@"
+docker-compose exec -u `id -u` php wp --path=/var/www/html --allow-root "$@"
